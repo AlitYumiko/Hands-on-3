61f7f4501026cb86134c9862925f27da596dfe69
AlitYumiko <116445405+AlitYumiko@users.noreply.github.com>
2025-04-10 02:28:10 -0600

    Add files via upload

---
+++ helloworld/helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello World"
+echo -e "\nContenido actual:"
+ls -l
+echo -e "\nCreando el directorio Test..."
+mkdir Test
+echo -e "\nAccediendo a Test..."
+cd Test
+echo -e "\nContenido de Test:"
+ls -l
